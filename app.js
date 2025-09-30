0ff362c3f3b1337997096d43f82ca5063f809692
Hikmet Mirzeyev <124464293+MageHiko@users.noreply.github.com>
2025-09-30 06:45:39 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,3 +2,25 @@
 $(document).ready(function () {
 
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const addButtons = document.querySelectorAll(".add .btn");
+  const popup = document.getElementById("card");
+  const closeBtn = document.querySelector(".close-btn");
+
+  addButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+      popup.style.display = "flex";
+    });
+  });
+
+  closeBtn.addEventListener("click", () => {
+    popup.style.display = "none";
+  });
+
+  window.addEventListener("click", (e) => {
+    if (e.target === popup) {
+      popup.style.display = "none";
+    }
+  });
+});
