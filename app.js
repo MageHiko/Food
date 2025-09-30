a7fd9ab45cbf86e740cca6c36eef678ceaae63f5
Hikmet Mirzeyev <124464293+MageHiko@users.noreply.github.com>
2025-09-30 06:56:32 +0400

    Update app.js

--- app.js
+++ app.js
@@ -5,22 +5,22 @@ $(document).ready(function () {
 
 document.addEventListener("DOMContentLoaded", () => {
   const addButtons = document.querySelectorAll(".add .btn");
-  const popup = document.getElementById("card");
+  const payment = document.getElementById("card");
   const closeBtn = document.querySelector(".close-btn");
 
   addButtons.forEach(btn => {
     btn.addEventListener("click", () => {
-      popup.style.display = "flex";
+      payment.style.display = "flex";
     });
   });
 
   closeBtn.addEventListener("click", () => {
-    popup.style.display = "none";
+    payment.style.display = "none";
   });
 
   window.addEventListener("click", (e) => {
-    if (e.target === popup) {
-      popup.style.display = "none";
+    if (e.target === payment) {
+      payment.style.display = "none";
     }
   });
 });
